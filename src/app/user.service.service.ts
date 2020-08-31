49d9e7fb596bfe38f08c4e95b81d40b84ac8089b
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-31 22:25:38 +0300

    modified files

--- src/app/user.service.service.ts
+++ src/app/user.service.service.ts
@@ -1,9 +1,94 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { environment } from '../environments/environment';
+import { Repo } from './repo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class User.ServiceService {
+export class UserServiceService {
+  newUser: User;
+  newRepo: Repo;
+  apiKey: string = environment.apiKey;
+  //apiUrl = environment.apiKey;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.newUser = new User('', '', '', '', '', '', 0, 0, 0, new Date());
+    this.newRepo = new Repo('', '', '', new Date(), '');
+  }
+
+  // getting user details from the server
+  getUserDetail(username: string) {
+    interface Profile {
+      url: string;
+      name: string;
+      email: string;
+      login: string;
+      html_url: string;
+      location: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+      avatar_url: string;
+      created_at: Date;
+    }
+
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<Profile>(
+          'https://api.github.com/users/' +
+            username +
+            '?access_token=' +
+            environment.apiKey
+        )
+        .toPromise()
+        .then(
+          (response) => {
+            this.newUser = response;
+            //console.log(this.newUser);
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+            reject(error);
+          }
+        );
+    });
+    return promise;
+  }
+
+  // getting repo info from the server
+
+  getRepoDetails(username) {
+    interface Repos {
+      name: string;
+      html_url: string;
+      description: string;
+      language: string;
+      created_at: Date;
+    }
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<Repos>(
+          'https://api.github.com/users/' +
+            username +
+            '/repos?access_token=' +
+            environment.apiKey
+        )
+        .toPromise()
+        .then(
+          (results) => {
+            this.newRepo = results;
+            //console.log(results);
+            resolve();
+          },
+          (error) => {
+            console.log(error);
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
 }
