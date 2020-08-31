809aa20180b19fbfcaec8842edae710dd505c2e7
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-31 22:47:18 +0300

    styled about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserServiceService } from './user-service.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RepoComponent } from './repo/repo.component';
 import { SearchFormComponent } from './search-form/search-form.component';
-import { UserComponent } from './user/user.component';
+//import { NgProgress, NgProgressModule } from '@ngx-progressbar/core';//
+//import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';//
 import { UnderlineDirective } from './underline.directive';
 import { DatePipePipe } from './date-pipe.pipe';
 
@@ -15,18 +20,22 @@ import { DatePipePipe } from './date-pipe.pipe';
   declarations: [
     AppComponent,
     AboutComponent,
+    UserComponent,
     NavbarComponent,
     RepoComponent,
     SearchFormComponent,
-    UserComponent,
     UnderlineDirective,
-    DatePipePipe
+    DatePipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+   // NgProgressModule.forRoot(),//
+    //NgProgressHttpClientModule,//
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [UserServiceService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/user.service.service.ts
+++ src/app/user.service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { Repo } from './repo';
 
 @Injectable({
