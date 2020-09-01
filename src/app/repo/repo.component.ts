b17facdabd4eaecfeee721aabdd12905e96f2503
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-01 10:51:55 +0300

    added styling

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from '../user.service.service';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
-  styleUrls: ['./repo.component.css']
+  styleUrls: ['./repo.component.css'],
 })
 export class RepoComponent implements OnInit {
+  searchUsers = true;
 
-  constructor() { }
+  repo: Repo;
+  reponame: string;
 
-  ngOnInit(): void {
+  constructor(private repoService: UserServiceService) {}
+
+  search(reponame) {
+    this.repoService.getRepoDetails(reponame).then(
+      (results) => {
+        this.repo = this.repoService.newRepo;
+        console.log(results);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit() {}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from '../user.service.service';
+import { User } from '../user';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  searchUsers = true;
 
-  constructor() { }
+  user: User;
+  repo: Repo;
+  username: string;
 
-  ngOnInit(): void {
+  constructor(
+    public userService: UserServiceService,
+    private repoService: UserServiceService
+  ) {}
+
+  getUser(username) {
+    this.userService.getUserDetail(username).then(
+      (results) => {
+        this.user = this.userService.newUser;
+        console.log(results);
+      },
+      (error) => {
+        alert('Enter valid username');
+        console.log(error);
+      }
+    );
+    this.repoService.getRepoDetails(username).then(
+      (results) => {
+        this.repo = this.repoService.newRepo;
+        console.log(results);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  ngOnInit() {
+    this.getUser('default-007');
+  }
 }
